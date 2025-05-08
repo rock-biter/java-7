87ecdf59de82a3103452dedd5c3f2826a6aa7cba
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-05-08 21:03:09 +0200

    add consiglio esercizio ;P

--- js-for-loop/js/app.js
+++ js-for-loop/js/app.js
@@ -113,3 +113,48 @@ for (let i = 0; i < 100; i += 2) {
 	// }
 	console.log('----------------')
 }
+
+/**
+ * @here
+Ciao ragazzi,
+Esercizio di oggi: FizzBuzz
+nome repo: js-fizzbuzz
+
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+Buon lavoro e buon divertimento! :slightly_smiling_face:
+
+1 => 1
+2 => 2
+3 => Fizz
+4 => 4
+5 => Buzz
+6 => Fizz
+7 => 7
+8 => 8
+9 => Fizz
+
+...
+
+15 => FizzBuzz
+ 
+...
+
+20 => Buzz
+21 => Fizz
+
+....
+30 => FizzBuzz
+
+ */
